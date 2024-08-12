a3ea633f66b82ba7c96568d1b967c1f4b7985a8f
Wanderson Kenedy <bolha.wanderson@grupofleury.com.br>
2024-08-12 09:34:39 -0300

    CHANGE: Revalidate from nextjs for work ISR on vercel

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,6 +4,9 @@ export const PUBLIC_API_LOCALHOST = process.env.NEXT_PUBLIC_BASE_URL_API;
 export async function fetchFromApi(url: string, options?: RequestInit) {
     const response = await fetch(url, {
         ...options,
+        next: {
+            revalidate: 0
+        },
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${process.env.AUTH_TOKEN || process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
